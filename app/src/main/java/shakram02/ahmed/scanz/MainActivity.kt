0d1a351ce3d4a0e2e244b5ba2dba39f27eda3925
shakram02 <ahmedhamdyau@gmail.com>
2017-12-26 22:17:04 +0200

    simple functionality

--- app/src/main/java/shakram02/ahmed/scanz/MainActivity.kt
+++ app/src/main/java/shakram02/ahmed/scanz/MainActivity.kt
@@ -1,16 +1,24 @@
 package shakram02.ahmed.scanz
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
+import android.net.wifi.WifiManager
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
+import butterknife.ButterKnife
+import butterknife.OnClick
 import com.esotericsoftware.kryonet.Client
 import com.google.zxing.client.android.Intents
 import com.google.zxing.integration.android.IntentIntegrator
 import java.net.InetAddress
 
+
 class MainActivity : Activity() {
+    private lateinit var wifiManager: WifiManager
+    private lateinit var wifiDialog: AlertDialog
+
     private val netThread: Thread = Thread({
         val client = Client()
         client.start()
@@ -25,16 +33,14 @@ class MainActivity : Activity() {
         const val UDP_PORT = 51213
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.connect_button).setOnClickListener {
-            val integrator = IntentIntegrator(this)
-            integrator.addExtra(Intents.Scan.QR_CODE_MODE, true)
-            integrator.initiateScan()
-        }
+        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        wifiDialog = createWifiDialog(this, wifiManager)
+
+        ButterKnife.bind(this)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
@@ -48,4 +54,17 @@ class MainActivity : Activity() {
             Toast.makeText(this, result.contents, Toast.LENGTH_SHORT).show()
         }
     }
+
+    @OnClick(R.id.connect_button)
+    fun connectInternet() {
+        val integrator = IntentIntegrator(this)
+        integrator.addExtra(Intents.Scan.QR_CODE_MODE, true)
+        integrator.initiateScan()
+//        if (!wifiManager.isWifiEnabled) {
+//            showToast(this, "Couldn't connect, please enable WiFi")
+//        } else {
+//            netThread.start()
+//        }
+    }
+
 }
---
+++ app/src/main/java/shakram02/ahmed/scanz/Utils.kt
@@ -0,0 +1,35 @@
+package shakram02.ahmed.scanz
+
+import android.app.AlertDialog
+import android.content.Context
+import android.net.wifi.WifiManager
+import android.widget.Toast
+
+/**
+ * Instantiates a Wifi control dialog
+ */
+internal fun createWifiDialog(context: Context, wifiManager: WifiManager): AlertDialog {
+
+//    val integrator = IntentIntegrator(this)
+//    integrator.addExtra(Intents.Scan.QR_CODE_MODE, true)
+//    integrator.initiateScan()
+
+    val alertDialogBuilder = AlertDialog.Builder(context)
+
+    // set title
+    alertDialogBuilder.setTitle("Wifi Settings")
+
+    // set dialog message
+    alertDialogBuilder
+            .setMessage("Do you want to enable WIFI ?")
+            .setCancelable(false)
+            .setPositiveButton("Yes") { _, _ -> wifiManager.isWifiEnabled = true }
+            .setNegativeButton("No") { _, _ -> wifiManager.isWifiEnabled = false }
+
+    // create alert dialog
+    return alertDialogBuilder.create()
+}
+
+fun showToast(context: Context, text: String) {
+    Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+}
