975c62ab855c2164b92af86593dc26c1b1dabe3f
shakram02 <ahmedhamdyau@gmail.com>
2017-12-26 21:08:44 +0200

    init

---
+++ app/src/main/java/shakram02/ahmed/scanz/MainActivity.kt
@@ -0,0 +1,12 @@
+package shakram02.ahmed.scanz
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
